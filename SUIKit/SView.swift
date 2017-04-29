55e62a8888c22b0fd91ddbc1b211fc8b74d20e7e
Kansujiya <sureshkansujiya@gmail.com>
2017-04-30 00:00:03 +0530

    Base classes Added

---
+++ SUIKit/SView.swift
@@ -0,0 +1,21 @@
+//
+//  SView.swift
+//  SUIKit
+//
+//  Created by Suresh Kansujiya on 29/04/17.
+//  Copyright © 2017 Suresh Kansujiya. All rights reserved.
+//
+
+import UIKit
+
+class SView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
